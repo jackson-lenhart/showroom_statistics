bbf5791bb829907a910f5cdec439c2ca9f2953dc
Jackson Lenhart <jacksonlenhart@gmail.com>
2018-09-14 01:25:26 -0400

    Hello world commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='showroom_statistics',
+      version='0.1',
+      description='Stats and analytics for sales activity on retail floor',
+      url='https://github.com/jackson-lenhart/showroom_statistics.git',
+      author='Jackson Lenhart',
+      author_email='jacksonlenhart@gmail.com',
+      license='MIT',
+      packages=['showroom_statistics'],
+      zip_safe=False)
